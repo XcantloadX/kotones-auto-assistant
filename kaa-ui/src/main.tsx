6e655bc773100464b66cd9a1120499385e5edb59
XcantloadX <3188996979@qq.com>
2025-10-13 21:45:05 +0800

    feat(ui): 禁用按钮默认展示禁用样式的光标

--- kaa-ui/src/main.tsx
+++ kaa-ui/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { ToastProvider } from './lib/toast.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
