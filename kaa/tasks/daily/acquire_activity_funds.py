3fe4d9b206bd127c41073ebeb482f7f74d494432
XcantloadX <3188996979@qq.com>
2026-07-30 22:10:39 +0800

    refactor(task): 重构领取活动费任务

--- kaa/tasks/daily/acquire_activity_funds.py
+++ kaa/tasks/daily/acquire_activity_funds.py
@@ -1,13 +1,49 @@
 """收取活动费"""
 import logging
 
-from kotonebot.backend.loop import Loop
+from kotonebot import task, device, color, sleep
+from kotonebot.pipeline import prefab as p, dummy, Pipeline, click_first, node, sleep as make_sleep
+
 from kaa.tasks import R
 from kaa.config import conf
-from ..actions.scenes import at_home, goto_home
-from kotonebot import task, device, color
+from ..actions.scenes import goto_home
 
 logger = logging.getLogger(__name__)
+
+@node
+def _go_home() -> bool:
+    goto_home()
+    return True
+
+def AcquireActivityFunds() -> Pipeline:
+    entry = dummy()
+    exit = dummy()
+
+    click_entry = dummy([lambda c: device.click(R.Daily.ActivityFunds.EntryArea)])
+    is_at_dialog = p(R.Daily.ActivityFunds.DialogTitle)
+    close_dialog = p(R.Daily.ActivityFunds.DialogButtonClose, [click_first, make_sleep(1)])
+
+    @node
+    def need_acquire() -> bool:
+        sleep(1)
+        needed = (
+            color.find('#ff6085', rect=R.Daily.ActivityFunds.EntryArea)
+            or color.find('#ff1249', rect=R.Daily.ActivityFunds.EntryArea)
+        ) is not None
+        return needed
+ 
+    _ = entry >> _go_home() >> [
+        need_acquire() >> [
+            click_entry,
+            is_at_dialog >> [
+                close_dialog,
+                _go_home() >> exit
+            ]
+        ],
+        exit
+    ]
+
+    return Pipeline(entry=entry, exit=exit)
 
 @task('收取活动费', screenshot_mode='manual-inherit')
 def acquire_activity_funds():
@@ -15,19 +51,7 @@ def acquire_activity_funds():
         logger.info('Activity funds acquisition is disabled.')
         return
 
-    if not at_home():
-        goto_home()
-    
-    for _ in Loop():
-        if (
-            not color.find('#ff1249', rect=R.Daily.BoxHomeActivelyFunds)
-            and at_home()
-        ): 
-            break
-        elif R.Common.ButtonClose.try_click():
-            logger.info('Closed popup dialog.')
-        else:
-            device.click(R.Daily.BoxHomeActivelyFunds)
+    AcquireActivityFunds().run(interval=1)
 
 if __name__ == '__main__':
     import logging
